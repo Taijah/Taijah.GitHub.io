44eb16f0b928c23ac83d7d0fbb354cd217bd9e32
Taijah <hindsrowe.t@gmail.com>
2016-03-15 11:41:11 -0400

    adding other functions , hashtags

--- OtherProjects/ajaxExamples/js/clients.js
+++ OtherProjects/ajaxExamples/js/clients.js
@@ -1,8 +1,18 @@
-
+$(document).ready(function(){
 $("#getClients").on("click",function(){
 
 $.getJSON("http://Taijah.GitHub.io/OtherProjects/ajaxExamples/jsonDatabase/clients.json" ,function(data){
-  alert(data);
-  console.dir(data);
-})
-})
+
+  $.each(data, function(index, item){
+    $("#data").append(item.name);
+
+
+  }) // each end
+  //alert(data);
+  //console.dir(data);
+
+})// get json end
+
+}) // click end
+
+})// doc end
