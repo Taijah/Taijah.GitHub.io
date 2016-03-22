b1a28779ffc9e9e591e713577fd90fc71068b818
Taijah <hindsrowe.t@gmail.com>
2016-03-22 12:05:10 -0400

    cvbnm

--- OtherProjects/ajaxExamples/js/question4.js
+++ OtherProjects/ajaxExamples/js/question4.js
@@ -2,28 +2,19 @@ $(document).ready(function(){
 $("#showInfo").on("click",function(){
 
 $.getJSON("http://Taijah.GitHub.io/OtherProjects/ajaxExamples/jsonDatabase/question4.json" ,function(data){
-
 var html="<table class='table table-hover table-striped'>"+
 "<tr><th>Name</th><th>address</th><th>phone</th><th>about</th></tr>";
-
-
-
-
-
   $.each(data, function(index, item){
   //  $("#data").append(item.name);
 
-
-
-  html+="<tr>"+
-  "<td>" +item.name +"</td>"+
-  "<td>"+item.address + "</td>"+
-  "<td>"+item.phone+ "</td>"+
-  "<td>"+item.about+ "</td>"+
-  "</tr>";
-
-
+html+="<tr>"+
+"<td>" +item.name +"</td>"+
+"<td>"+item.address + "</td>"+
+"<td>"+item.phone+ "</td>"+
+"<td>"+item.about+ "</td>"+
+"</tr>";
   }) // each end
+  
   html+="</table>";
   $("#data").append(html);
 
