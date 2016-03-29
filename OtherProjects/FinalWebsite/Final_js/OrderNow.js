cc7a404526dfab638d0fda75bcc4a8372bf3a808
Taijah <hindsrowe.t@gmail.com>
2016-03-29 00:20:22 -0400

    mouse enter button

--- OtherProjects/FinalWebsite/Final_js/OrderNow.js
+++ OtherProjects/FinalWebsite/Final_js/OrderNow.js
@@ -1,6 +1,14 @@
+// button to change when mouse is over button
 $("#myButton").on("mouseenter", function(){
 
   $(this).text("Create Outfit!");
 
 
 })
+
+// when mouse is off of button
+.on("mouseleave", function(){
+
+      $(this).text("Click Me!");
+
+});
