df41357c53ebe20c7d9c7b0b8d6737131fb04eea
Taijah <hindsrowe.t@gmail.com>
2016-03-29 09:52:12 -0400

    hfgjtyuk

--- OtherProjects/FinalWebsite/Final_js/About.js
+++ OtherProjects/FinalWebsite/Final_js/About.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
 // button to change when mouse is over button
 //$("#like").on("Click", function(){
 
-alert("Thank You For Liking Us on Facebok");
-
-
-
 // when mouse is off of button
 //.on("mouseleave", function(){
 
     //  $(this).text("Click Me!");
+
+$("like").on("click", function() 
+alert("Thank You For Liking Us on Facebok");
+
+
+
 
 })
