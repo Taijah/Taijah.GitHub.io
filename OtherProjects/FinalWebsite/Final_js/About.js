42a2b8988a5eb72cb4303f7144d30873a478c215
Taijah <hindsrowe.t@gmail.com>
2016-03-29 00:39:50 -0400

    added alert

---
+++ OtherProjects/FinalWebsite/Final_js/About.js
@@ -0,0 +1,14 @@
+// button to change when mouse is over button
+$("#like").on("Click", function(){
+
+alert(Thank You For Liking Us on Facebok!);
+
+
+})
+
+// when mouse is off of button
+//.on("mouseleave", function(){
+
+      $(this).text("Click Me!");
+
+});
