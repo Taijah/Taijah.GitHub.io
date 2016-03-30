822a481b6a407fb94f942f89bde37a93dd876d18
Taijah <hindsrowe.t@gmail.com>
2016-03-29 22:26:25 -0400

    added contents

--- OtherProjects/ajaxExamples/js/question4.js
+++ OtherProjects/ajaxExamples/js/question4.js
@@ -1,37 +1,38 @@
 $(document).ready(function(){
-$("#showInfo").on("click",function(){
+  $("#showInfo").on("click",function(){
 
-$.getJSON("jsonDatabase/question4.json", function(data) {
+    $.getJSON("jsonDatabase/question4.json", function(data) {
 
-            console.dir(data);
-            var html = "";
+                console.dir(data);
+                var html = "";
 
-            $.each(data, function(index, item) {
-                html += '<div class="col-md-4">' +
-                  '<div class="Name">' + item.name + '</div>' +
-                  '<div class="company"><small>company </small>' + item.company + '</div>' +
-                  '<div class="address"><small>address</small>' + item.adress + '</div>' +
-                  '<div class="age"><small>age </small>' + item.age + '</div>' +
-                  //deleted commentsContainer
-                  '<div class="panel panel-default">' + //added
-                  '<div class="panel-heading">Renter Comments</div>'; //added
-                $.each(item.comments, function(ind, i) {
-                    html += '<div class="panel-body">' + //added
-                      '<div class="renterName">' + i.username + '</div>' +
-                      '<div class="renterComment">' + i.comment + '</div>' +
-                      '<div class="renterStars">';
+                $.each(data, function(index, item) {
+                    html += '<div class="col-md-4">' +
+                      '<div class="Name">' + item.name + '</div>' +
+                      '<div class="company"><small>company </small>' + item.company + '</div>' +
+                      '<div class="address"><small>address</small>' + item.adress + '</div>' +
+                      '<div class="age"><small>age </small>' + item.age + '</div>' +
+                      //deleted commentsContainer
+                      '<div class="panel panel-default">' + //added
+                      '<div class="panel-heading">Renter Comments</div>'; //added
+                    $.each(item.comments, function(ind, i) {
+                        html += '<div class="panel-body">' + //added
+                          '<div class="renterName">' + i.username + '</div>' +
+                          '<div class="renterComment">' + i.comment + '</div>' +
+                          '<div class="renterStars">';
 
 
 
-                      '</div>'; //panel body
-                  }) //eeach fact
-                  html+="</table>";
-                  $("#data").append(html);
+                          '</div>'; //panel body
+                      }) //eeach fact
+                      html+="</table>";
+                      $("#data").append(html);
 
-                html += '</div>' + //panel
-                  '</div>'; //col-md-4
-              }) //each person
+                    html += '</div>' + //panel
+                      '</div>'; //col-md-4
+                  }) //each person
 
-            }) // click end
 
-          })// doc end
+
+}) //click end
+}) // doc end
